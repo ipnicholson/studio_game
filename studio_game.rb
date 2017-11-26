447203ded604a6bef72dfd861be950303ca45775
Ian Nicholson <ipnicholson@gmail.com>
2017-11-26 15:20:18 -0800

    exercise 3

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,10 @@
-greeting = "Welcome!"
+name1 = 'larry'
+health1 = 60
 
-3.times do
-  puts greeting.upcase
-end
+name2 = 'curly'
+name3 = 'moe'
 
-puts Time.new
+puts "#{name1}'s health is #{health1 / 9.0}"
+
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
