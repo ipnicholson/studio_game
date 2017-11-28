e37a6e87f85ae52e7f7b8d4b41a607857d9936ec
Ian Nicholson <ipnicholson@gmail.com>
2017-11-28 12:50:53 -0800

    exercise 6

--- studio_game.rb
+++ studio_game.rb
@@ -1,26 +1,13 @@
-name1 = 'larry'
-health1 = 60
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-name2 = 'curly'
-health2 = health1
+def time 
+  Time.new.strftime("%T")
+end
 
-health1 = 30
-
-name3 = 'moe'
-health3 = 100
-
-name4 = 'shemp'
-health4 = 90
-
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-puts "#{name4.capitalize}".ljust(30, '.') + " #{health4} health"
-
-puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
-
-start_time = Time.new
-formatted_time = start_time.strftime('%A %m/%d/%Y at %I:%M%p')
-
-puts "The game started on #{formatted_time}"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
 
