e80aebc62991f6db6f8b7d48e16b54062d3535ef
Ian Nicholson <ipnicholson@gmail.com>
2017-11-30 10:34:42 -0800

    exercise 8

--- studio_game.rb
+++ studio_game.rb
@@ -4,8 +4,15 @@ class Player
     @health = health
   end
 
+  attr_accessor :name
+  attr_reader :health
+
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+
+  def score
+    @name.length + @health
   end
 
   def blam
@@ -17,18 +24,21 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
 end
 
 player1 = Player.new('moe')
 puts player1
 
 player2 = Player.new('larry', 60)
-puts player2
+puts player2.name
 
-player3 = Player.new('curly', 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
-player2.blam
+player2.name = 'lawrence'
+puts player2.name
+puts player2.health
+puts player2.score
+puts player2
