4e521013ad7ff0aca48c884a396682e227aba9ad
Ian Nicholson <ipnicholson@gmail.com>
2017-12-11 15:35:35 -0800

    initial commit

--- game_spec.rb
+++ game_spec.rb
@@ -9,8 +9,32 @@ describe Game do
     @player = Player.new("moe", @initial_health)
 
     @game.add_player(@player)
+    $stdout = StringIO.new
   end
 
+  it "w00ts the player when a high number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(5)
 
+    @game.play
+
+    @player.health.should == @initial_health + 15
+  end
+
+  it "skips the player when a medium number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(3)
+
+    @game.play
+
+    @player.health.should == @initial_health
+
+  end
+
+  it "blams the player when a low number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(1)
+
+    @game.play
+
+    @player.health.should == @initial_health - 10
+  end
 
 end
