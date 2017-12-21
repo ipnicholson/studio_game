71c30a3800f2418a56f023e4b1e2358a4c6ac432
Ian Nicholson <ipnicholson@gmail.com>
2017-12-21 15:25:18 -0800

    refactor to move random play behavior into module GameTurn

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 require_relative 'player'
-require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   def initialize(title)
@@ -15,25 +15,13 @@ class Game
 
   def play
     puts "There are #{@players.size} players in #{@title}"
+    
     @players.each do |player|
       puts player
     end
 
     @players.each do |player|
-      die = Die.new
-      
-      case die.roll 
-      when 1..2 
-        player.blam
-      when 3..4 
-        puts "#{player.name} was skipped."
-      else 
-        player.w00t
-      end
-      puts player
-
+      GameTurn.take_turn(player)
     end
-
   end
-
 end
