63319db273aca011691bcd441bedfd7fc22aa954
Ian Nicholson <ipnicholson@gmail.com>
2017-12-22 13:59:55 -0800

    initial commit

---
+++ game_turn_spec.rb
@@ -0,0 +1,12 @@
+require_relative 'game_turn'
+require_relative 'spec_helper'
+
+# before do
+
+#   # it "selects a random treasure for a player's turn" do
+#   #   @game.play(1)
+
+#   #   expect()
+#   # end
+
+# end
