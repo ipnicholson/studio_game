6381e65a5c410dc196e803ff4222d0390fe5d35e
Ian Nicholson <ipnicholson@gmail.com>
2017-12-22 14:00:07 -0800

    add random treasure

--- game_turn.rb
+++ game_turn.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'treasure_trove'
 
 module GameTurn
   def self.take_turn(player)
@@ -14,5 +15,9 @@ module GameTurn
       player.w00t
     end
     puts player
+
+    treasure = TreasureTrove.random
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
+
   end
 end
