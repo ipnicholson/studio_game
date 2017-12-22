ad565b5508faf8f1e707e01f73568d229edfdfe8
Ian Nicholson <ipnicholson@gmail.com>
2017-12-21 18:17:26 -0800

    update to test for rounds of play

--- game_spec.rb
+++ game_spec.rb
@@ -4,6 +4,7 @@ describe Game do
 
   before do
     @game = Game.new('Knuckleheads')
+    @rounds = 2
 
     @initial_health = 100
     @player = Player.new("moe", @initial_health)
@@ -15,15 +16,15 @@ describe Game do
   it "w00ts the player when a high number is rolled" do
     allow_any_instance_of(Die).to receive(:roll).and_return(5)
 
-    @game.play
+    @game.play(@rounds)
 
-    @player.health.should == @initial_health + 15
+    @player.health.should == @initial_health + (15 * @rounds)
   end
 
   it "skips the player when a medium number is rolled" do
     allow_any_instance_of(Die).to receive(:roll).and_return(3)
 
-    @game.play
+    @game.play(@rounds)
 
     @player.health.should == @initial_health
 
@@ -32,9 +33,9 @@ describe Game do
   it "blams the player when a low number is rolled" do
     allow_any_instance_of(Die).to receive(:roll).and_return(1)
 
-    @game.play
+    @game.play(@rounds)
 
-    @player.health.should == @initial_health - 10
+    @player.health.should == @initial_health - (10 * @rounds)
   end
 
 end
