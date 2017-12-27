b637698ca815aa5b747189de7b2f3fd65e7bfb8a
Ian Nicholson <ipnicholson@gmail.com>
2017-12-27 14:47:03 -0800

    remove hard-coded players

--- studio_game.rb
+++ studio_game.rb
@@ -2,13 +2,7 @@ require_relative 'game' # game requires player.rb, so that dependency is include
 
 knuckleheads = Game.new("Knuckleheads")
 
-player1 = Player.new('moe')
-player2 = Player.new('larry', 60)
-player3 = Player.new('curly', 125)
-
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
+knuckleheads.load_players('players.csv')
 
 loop do
   puts "\nHow many game rounds? ('quit' to exit)"
