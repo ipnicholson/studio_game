4fe19ef67a24edd8325e5a498c8cafed7c6c5857
Ian Nicholson <ipnicholson@gmail.com>
2017-12-30 13:04:49 -0800

    refactor

--- clumsy_player_spec.rb
+++ clumsy_player_spec.rb
@@ -3,28 +3,28 @@ require_relative 'spec_helper'
 
 describe ClumsyPlayer do
   before do
-    @clumsy = ClumsyPlayer.new("klutz", 100)
+    @clumsy_player = ClumsyPlayer.new("klutz", 100)
     $stdout = StringIO.new
   end
 
   it "creates a clumsy player" do
-    expect(@clumsy).to_not eq(nil)
+    expect(@clumsy_player).to_not eq(nil)
   end
 
   it "gets only half the point values from treasure" do
     hammer = Treasure.new(:hammer, 50)
-    @clumsy.found_treasure(hammer)
-    @clumsy.found_treasure(hammer)
+    @clumsy_player.found_treasure(hammer)
+    @clumsy_player.found_treasure(hammer)
 
-    expect(@clumsy.points).to eq(50)
+    expect(@clumsy_player.points).to eq(50)
 
     crowbar = Treasure.new(:crowbar, 400)
-    @clumsy.found_treasure(crowbar)
+    @clumsy_player.found_treasure(crowbar)
 
-    expect(@clumsy.points).to eq(250)
+    expect(@clumsy_player.points).to eq(250)
 
     yielded = []
-    @clumsy.each_found_treasure do |treasure|
+    @clumsy_player.each_found_treasure do |treasure|
       yielded << treasure
     end
 
