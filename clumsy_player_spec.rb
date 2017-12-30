f4c9b03ef7cb91af5e99ef7c385a1627afbe218a
Ian Nicholson <ipnicholson@gmail.com>
2017-12-30 10:51:11 -0800

    add test for ClumsyPlayer

--- clumsy_player_spec.rb
+++ clumsy_player_spec.rb
@@ -11,7 +11,23 @@ describe ClumsyPlayer do
     expect(@clumsy).to_not eq(nil)
   end
 
-  # it "gets only half the point values from treasure" do
+  it "gets only half the point values from treasure" do
+    hammer = Treasure.new(:hammer, 50)
+    @clumsy.found_treasure(hammer)
+    @clumsy.found_treasure(hammer)
 
-  # end
+    expect(@clumsy.points).to eq(50)
+
+    crowbar = Treasure.new(:crowbar, 400)
+    @clumsy.found_treasure(crowbar)
+
+    expect(@clumsy.points).to eq(250)
+
+    yielded = []
+    @clumsy.each_found_treasure do |treasure|
+      yielded << treasure
+    end
+
+    expect(yielded).to eq([Treasure.new(:hammer, 50), Treasure.new(:crowbar, 200)])
+  end
 end
