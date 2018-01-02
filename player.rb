1bae4f9ea71b5375fe37af4ee2fde6a87ea55272
Ian Nicholson <ipnicholson@gmail.com>
2018-01-02 14:53:03 -0800

    move w00t, blam, and strong? methods from Playable module

--- player.rb
+++ player.rb
@@ -1,6 +1,9 @@
+require_relative 'playable'
 require_relative 'treasure_trove'
 
 class Player
+  include Playable
+
   attr_accessor :name
   attr_reader :health
   
@@ -18,23 +21,13 @@ class Player
     other_player.score <=> score
   end
 
-  def strong?
-    @health > 100
-  end
+  
 
   def score
     @health + points
   end
 
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
+  
 
   def name=(new_name)
     @name = new_name.capitalize
