d52e7c970cdfe5646e509780a887214776119d6e
Ian Nicholson <ipnicholson@gmail.com>
2018-01-02 17:57:08 -0800

    refactor to use attributes and not instance variables

--- playable.rb
+++ playable.rb
@@ -1,16 +1,16 @@
 module Playable
 
   def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
+    self.health += 15
+    puts "#{name} got w00ted!"
   end
   
   def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
+    self.health -= 10
+    puts "#{name} got blammed!"
   end
   
   def strong?
-    @health > 100
+    health > 100
   end
 end
